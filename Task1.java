60e494b5b9be948b7cb0b06a450dac81076cc7b9
knyazevs <48366704+knyazevs@users.noreply.github.com>
2024-10-09 10:21:07 +0300

    Первая итерация

---
+++ Task1.java
@@ -0,0 +1,28 @@
+/*
+Задание 1
+
+Напишите вывод строки HelloWorld
+
+* Output:
+* Hello, World!
+
+Доп задание:
+Замените текст на ФИ и группу
+
+
+* Output:
+* Иванов Иван ИВ01
+*/
+
+public class Task1 {
+  public static void main(String args[]) {
+  }
+}
+
+/*
+
+Вспомогательные материалы:
+https://metanit.com/java/tutorial/2.11.php
+https://metanit.com/java/tutorial/2.9.php
+
+*/
